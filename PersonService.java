48ac7b604b5ff4e962f1387135779fa44ebea9ff
gena03111975 <gepota1@tut.by>
2024-11-23 10:16:15 +0300

    добавили PersonService.java

---
+++ PersonService.java
@@ -0,0 +1,28 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person createOrUpdatePerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+    public Optional<Person> getPersonById(Long id) {
+        return personRepository.findById(id);
+    }
+
+    public void deletePerson(Long id) {
+        personRepository.deleteById(id);
+    }
+}
